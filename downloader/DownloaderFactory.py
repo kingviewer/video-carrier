58f3f9934ad7d9c788e5c98264421a5b733d6b60
zxsd-mengqw <keven@lonzh.com>
2023-02-03 19:31:22 +0800

    增加注释

--- downloader/DownloaderFactory.py
+++ downloader/DownloaderFactory.py
@@ -5,6 +5,13 @@ from downloader.DouYinDownloader import DouYinDownloader
 class DownloaderFactory:
     @staticmethod
     def create_downloader(plat, video_id, proxy=None):
+        """
+        生成下载器
+        :param plat: 平台，目前支持 DouYin(抖音)、YouTube(油管)
+        :param video_id:
+        :param proxy:
+        :return: 下载器对象
+        """
         if plat.lower() == 'youtube':
             return YouTubeDownloader(video_id, proxy)
         if plat.lower() == 'douyin':
